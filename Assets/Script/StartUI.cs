6a74bc50e367ac1dea2a043ce20c523015ff233c
Mors <57664245+Morsiusiurandum@users.noreply.github.com>
2021-03-25 23:01:02 +0800

    Update StartScene

---
+++ Assets/Script/StartUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUI : MonoBehaviour
+{
+    private AsyncOperation async;
+    void Start()
+    {
+        StartCoroutine(loading());
+    }
+    void Update()
+    {
+        Debug.Log(async.progress * 100);
+    }
+    public void GameStart()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    IEnumerator loading()
+    {
+        async = SceneManager.LoadSceneAsync("GameScene");
+
+        yield return async;
+
+    }
+}
